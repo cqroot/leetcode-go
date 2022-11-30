af5fce496f08c8d175adba0a66196cd0f399a6d3
cqroot <cqroot@outlook.com>
2022-11-30 16:09:19 +0800

    feat(P1): separate the two solutions

--- P1_two-sum/solution_hash_table.go
+++ P1_two-sum/solution_hash_table.go
@@ -1,19 +1,5 @@
 package solution
 
-// Brute Force
-// Time complexity: O(n^2)
-// Space complexity: O(1)
-func twoSum_BruteForce(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
-		}
-	}
-	return nil
-}
-
 // Hash Table
 // Time complexity: O(n)
 // Space complexity: O(n)
