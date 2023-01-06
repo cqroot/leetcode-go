4fbe46ddd1062f9d3aac3bebdc2ba5145d2dc369
cqroot <cqroot@outlook.com>
2023-01-06 16:07:15 +0800

    feat(P0080): initial commit

---
+++ solutions/P0080_remove-duplicates-from-sorted-array-ii/solution_test.go
@@ -0,0 +1,72 @@
+package solution
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type input = []int
+
+type expected struct {
+	nums []int
+	val  int
+}
+
+type testcase struct {
+	input    input
+	expected expected
+}
+
+func TestRemoveDuplicates(t *testing.T) {
+	testcases := []testcase{
+		{
+			input: []int{1, 1, 1, 2, 2, 3},
+			expected: expected{
+				nums: []int{1, 1, 2, 2, 3},
+				val:  5,
+			},
+		},
+		{
+			input: []int{0, 0, 1, 1, 1, 1, 2, 3, 3},
+			expected: expected{
+				nums: []int{0, 0, 1, 1, 2, 3, 3},
+				val:  7,
+			},
+		},
+		{
+			input: []int{1},
+			expected: expected{
+				nums: []int{1},
+				val:  1,
+			},
+		},
+	}
+
+	for _, testcase := range testcases {
+		fTest := func(actualVal int, actualNums []int) {
+			expectedNums := testcase.expected.nums
+
+			sort.Slice(actualNums[:actualVal], func(i, j int) bool {
+				return actualNums[i] < actualNums[j]
+			})
+			sort.Slice(expectedNums, func(i, j int) bool {
+				return expectedNums[i] < expectedNums[j]
+			})
+
+			assert.Equal(t, testcase.expected.val, actualVal)
+			for i := 0; i < testcase.expected.val; i++ {
+				assert.Equal(t, expectedNums[i], actualNums[i])
+			}
+		}
+
+		actualNums := make([]int, len(testcase.input))
+
+		copy(actualNums, testcase.input)
+		fTest(
+			removeDuplicates_TwoPointers(actualNums),
+			actualNums,
+		)
+	}
+}
--- solutions/P0080_remove-duplicates-from-sorted-array-ii/solution_two_pointers.go
+++ solutions/P0080_remove-duplicates-from-sorted-array-ii/solution_two_pointers.go
@@ -7,11 +7,18 @@ package solution
 func removeDuplicates_TwoPointers(nums []int) int {
 	left, right := 0, 1
 
+	first := true
 	for right < len(nums) {
 		if nums[right] != nums[left] {
 			left++
 			nums[left] = nums[right]
+			first = true
+		} else if first {
+			left++
+			nums[left] = nums[right]
+			first = false
 		}
+
 		right++
 	}
 
