9f813df5b750a9fb175f8a179060ff28b4fef7d4
Keith Chu <cqroot@outlook.com>
2023-04-24 20:43:40 +0800

    ✨ 14. Longest Common Prefix

---
+++ 99-unsorted/p0014-longest-common-prefix/solution_test.go
@@ -0,0 +1,31 @@
+package solution
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type testcase struct {
+	input    []string
+	expected string
+}
+
+func TestLongestCommonPrefix(t *testing.T) {
+	testcases := []testcase{
+		{
+			input:    []string{"flower", "flow", "flight"},
+			expected: "fl",
+		},
+		{
+			input:    []string{"dog","racecar","car"},
+			expected: "",
+		},
+	}
+
+	for _, testcase := range testcases {
+		require.Equal(t, testcase.expected,
+			longestCommonPrefix(testcase.input),
+			"Testcase: %+v", testcase.input)
+	}
+}
--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@ test:
 	@# Make sure these lines in the template are modified
 	@sh -c "if grep -rn 'TestAdd(t \*testing.T)' 0*/; then exit 1; fi"
 	@sh -c "if grep -rn 'TestAdd(t \*testing.T)' 9*/; then exit 1; fi"
-	@sh -c "if grep -rn '^// Name$$' solutions/; then exit 1; fi"
+	@sh -c "if grep -rn '^// Name$$' 0*/; then exit 1; fi"
+	@sh -c "if grep -rn '^// Name$$' 9*/; then exit 1; fi"
 	go test -v ./...
 
 .PHONY: check
